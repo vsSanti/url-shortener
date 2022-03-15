6e710ade21bd083778a8d075e8bdf74b3a9fc138
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-15 17:33:36 -0300

    test: ensure AddShortUrlController returns 400 if Validation returns an error

--- tests/presentation/controllers/add-short-url-controller.spec.ts
+++ tests/presentation/controllers/add-short-url-controller.spec.ts
@@ -1,6 +1,7 @@
 import faker from '@faker-js/faker';
 
 import { AddShortUrlController } from '@/presentation/controllers';
+import { badRequest } from '@/presentation/helpers';
 
 import { ValidationSpy } from '@/tests/domain/mocks';
 
@@ -24,5 +25,11 @@ describe('AddShortUrl Controller', () => {
   it('should call Validation with correct params', async () => {
     await sut.handle(params);
     expect(validationSpy.params).toEqual(params.body);
+  });
+
+  it('should return 400 if Validation returns an error', async () => {
+    validationSpy.error = new Error();
+    const response = await sut.handle(params);
+    expect(response).toEqual(badRequest(validationSpy.error));
   });
 });
