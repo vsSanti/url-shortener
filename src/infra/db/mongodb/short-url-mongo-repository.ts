85e1b3e29c26a566d4ff5ebfbc252d04354a3446
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-15 19:16:06 -0300

    feat: ensure ShortUrlMongoRepository returns a short url on findByAlias\ success

--- src/infra/db/mongodb/short-url-mongo-repository.ts
+++ src/infra/db/mongodb/short-url-mongo-repository.ts
@@ -1,9 +1,11 @@
 import { FindShortUrlByAliasRepository } from '@/data/protocols';
+import { ShortUrlMongoModel } from './schemas';
 
 export class ShortUrlMongoRepository implements FindShortUrlByAliasRepository {
   async findByAlias({
     alias,
   }: FindShortUrlByAliasRepository.Params): Promise<FindShortUrlByAliasRepository.Result> {
-    return undefined;
+    const shortUrl = await ShortUrlMongoModel.findOne({ alias });
+    return shortUrl ?? undefined;
   }
 }
