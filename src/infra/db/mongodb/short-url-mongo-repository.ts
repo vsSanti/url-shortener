55285ee925905e736acaec89f2573951b949e0cc
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-15 19:09:19 -0300

    feat: ensure ShortUrlMongoRepository returns undefined if nothing is found

--- src/infra/db/mongodb/short-url-mongo-repository.ts
+++ src/infra/db/mongodb/short-url-mongo-repository.ts
@@ -4,6 +4,6 @@ export class ShortUrlMongoRepository implements FindShortUrlByAliasRepository {
   async findByAlias({
     alias,
   }: FindShortUrlByAliasRepository.Params): Promise<FindShortUrlByAliasRepository.Result> {
-    throw new Error();
+    return undefined;
   }
 }
