386a8ce9dea85a5b5c76899495c37e8af1b17c68
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-15 19:16:19 -0300

    feat: add timestamps to ShortUrlMongoModel

--- src/infra/db/mongodb/schemas/short-url-mongo-schema.ts
+++ src/infra/db/mongodb/schemas/short-url-mongo-schema.ts
@@ -5,15 +5,18 @@ interface ShortUrl {
   alias: string;
 }
 
-const shortUrlSchema = new mongoose.Schema<ShortUrl>({
-  url: {
-    type: String,
-    required: true,
+const shortUrlSchema = new mongoose.Schema<ShortUrl>(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    alias: {
+      type: String,
+      required: true,
+    },
   },
-  alias: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const ShortUrlMongoModel = mongoose.model<ShortUrl>('ShortUrl', shortUrlSchema, 'short-url');
