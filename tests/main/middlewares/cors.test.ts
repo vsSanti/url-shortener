28162498112868a9940a2eb0bf58e264e3b83f63
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-15 23:53:18 -0300

    fix: tests who use supertest must open and close mongoose connection

--- tests/main/middlewares/cors.test.ts
+++ tests/main/middlewares/cors.test.ts
@@ -2,12 +2,18 @@ import { Express } from 'express';
 import request from 'supertest';
 
 import { setupApp } from '@/main/config/app';
+import { MongoHelper } from '@/infra/db/mongodb';
 
 describe('CORS Middleware', () => {
   let app: Express;
 
   beforeAll(async () => {
     app = await setupApp();
+    await MongoHelper.connect();
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
   });
 
   test('should enable CORS', async () => {
