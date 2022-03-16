b445944609d818706ce9f960c2d7bbd14264f216
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-15 22:45:13 -0300

    test: ensure LoadShortUrlController returns 400 if Validation returns an error

--- tests/presentation/controllers/load-short-url-controller.spec.ts
+++ tests/presentation/controllers/load-short-url-controller.spec.ts
@@ -1,6 +1,7 @@
 import faker from '@faker-js/faker';
 
 import { LoadShortUrlController } from '@/presentation/controllers';
+import { badRequest } from '@/presentation/helpers';
 
 import { ValidationSpy } from '@/tests/domain/mocks';
 
@@ -26,5 +27,11 @@ describe('LoadShortUrl Controller', () => {
   it('should call Validation with correct params', async () => {
     await sut.handle(params);
     expect(validationSpy.params).toEqual(params.params);
+  });
+
+  it('should return 400 if Validation returns an error', async () => {
+    validationSpy.error = new Error();
+    const response = await sut.handle(params);
+    expect(response).toEqual(badRequest(validationSpy.error));
   });
 });
