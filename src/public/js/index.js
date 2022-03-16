72ea5bc957b65f00f1cf74d09c4bc216b408be29
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-16 13:21:18 -0300

    feat: add copy short url to clipboard functionality

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -7,13 +7,21 @@ function handleSignupFormSubmit(e) {
   const formDataEntries = new FormData(signupForm).entries();
   const { url } = Object.fromEntries(formDataEntries);
 
-  console.log({ url });
+  if (!document.getElementById('#btn-copy-url-to-clipboard').classList.contains('hidden')) {
+    document.getElementById('#btn-copy-url-to-clipboard').classList.add('hidden')
+  }
 
   fetch(`${origin}/short-url`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ url }),
   }).then(async (data) => {
-    console.log(await data.json());
+    alias = (await data.json()).alias;
+    document.getElementById('#btn-copy-url-to-clipboard').classList.remove('hidden');
   });
+}
+
+function copyShortUrlToClipboard() {
+  navigator.clipboard.writeText(`${origin}/${alias}`);
+  alert('Link copied to clipboard!');
 }
