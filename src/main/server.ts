f29d223bffa5df87777098272559ad25992168ed
Vinicius Santiago <viniciussdsilva@gmail.com>
2022-03-15 22:26:40 -0300

    chore: setup mongo connection for docker and express server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,9 @@
 import env from '@/main/config/env';
 
-const initExpressServer = async () => {
+import { MongoHelper } from '@/infra/db/mongodb';
+
+MongoHelper.connect().then(async () => {
   const { setupApp } = await import('./config/app');
   const app = await setupApp();
   app.listen(env.port, () => console.log(`🚀 Server running at http://localhost:${env.port}`));
-};
-
-initExpressServer();
+});
